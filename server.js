ff8279c961f3a29e38d2f1b55b8a8efd36d3154d
Jason Crout <croutjason@gmail.com>
2022-10-09 16:46:09 +1100

    fix: edit port

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-app.listen(3000, function () {
-  console.log("Server started successfully on port 3000!");
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`Server started successfully on port ${port}!`);
 });
