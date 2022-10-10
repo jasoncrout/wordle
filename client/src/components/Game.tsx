30a6d9f2a42457b8b562d290e4cf05e38c574a3e
Jason Crout <croutjason@gmail.com>
2022-10-11 08:55:39 +1100

    feat: game can check win and lose

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { words } from "../static/words";
 import Board from "./Board";
 import "./Game.css";
 import Keyboard from "./Keyboard";
 
 function Game() {
+  const [answer, setAnswer] = useState("");
+  useEffect(() => {
+    const randomWord = words[(Math.random() * words.length) | 0];
+    document.title = randomWord;
+    setAnswer(randomWord);
+  }, []);
+
   const [board, setBoard] = useState([
     [{ value: "" }, { value: "" }, { value: "" }, { value: "" }, { value: "" }],
     [{ value: "" }, { value: "" }, { value: "" }, { value: "" }, { value: "" }],
@@ -13,38 +21,73 @@ function Game() {
     [{ value: "" }, { value: "" }, { value: "" }, { value: "" }, { value: "" }],
   ]);
 
-  const [row] = useState(0);
+  const [row, setRow] = useState(0);
+
+  const addLetter = (letter: string) => {
+    setBoard((prevState) => {
+      const currentBoard = [...prevState];
+      for (let col = 0; col < board[row].length; col++) {
+        if (currentBoard[row][col].value === "") {
+          currentBoard[row][col].value = letter;
+          break;
+        }
+      }
+      return currentBoard;
+    });
+  };
+
+  const removeLetter = () => {
+    setBoard((prevState) => {
+      const currentBoard = [...prevState];
+      for (let col = 0; col < board[row].length; col++) {
+        if (currentBoard[row][col].value === "") {
+          if (col === 0) {
+            currentBoard[row][col].value = "";
+          } else {
+            currentBoard[row][col - 1].value = "";
+          }
+          break;
+        }
+        if (col === board[row].length - 1) {
+          currentBoard[row][col].value = "";
+        }
+      }
+      return currentBoard;
+    });
+  };
+
+  const enter = () => {
+    let word = "";
+    for (let col = 0; col < board[row].length; col++) {
+      if (board[row][col].value === "") {
+        return null;
+      } else {
+        word += board[row][col].value;
+      }
+    }
+    return word;
+  };
+
+  const checkWin = (guess: string) => {
+    if (guess.toLowerCase() === answer.toLowerCase()) {
+      alert("You win!");
+    } else if (row < board.length - 1) {
+      setRow((prevRow) => prevRow + 1);
+    } else {
+      alert("You lose!");
+    }
+  };
 
   const setBoardWrapper = (value: string) => {
-    if (!(value === "⌫")) {
-      setBoard((prevState) => {
-        const currentBoard = [...prevState];
-        for (let col = 0; col < board[row].length; col++) {
-          if (currentBoard[row][col].value === "") {
-            currentBoard[row][col].value = value;
-            break;
-          }
-        }
-        return currentBoard;
-      });
+    if (value === "⌫") {
+      removeLetter();
+    } else if (value === "enter") {
+      const word = enter();
+      if (word) {
+        checkWin(word);
+      }
     } else {
-      setBoard((prevState) => {
-        const currentBoard = [...prevState];
-        for (let col = 0; col < board[row].length; col++) {
-          if (currentBoard[row][col].value === "") {
-            if (col === 0) {
-              currentBoard[row][col].value = "";
-            } else {
-              currentBoard[row][col - 1].value = "";
-            }
-            break;
-          }
-          if (col === board[row].length - 1) {
-            currentBoard[row][col].value = "";
-          }
-        }
-        return currentBoard;
-      });
+      addLetter(value);
     }
   };
 
